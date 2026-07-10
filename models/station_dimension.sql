b53faaa7e08fd65dbfbc8e4d88061b3f6ae319b5
Luis Hernandez <hedez.luis94@gmail.com>
2026-07-10 03:22:07 +0000

    station_dimesion model created

---
+++ models/station_dimension.sql
@@ -0,0 +1,15 @@
+WITH BIKE AS
+(
+    SELECT
+    DISTINCT
+    START_STATIO_ID AS station_id,
+    START_STATION_NAME AS station_name,
+    START_LAT AS station_lat,
+    START_LNG AS start_station_lng
+    FROM {{ source('demo', 'bike') }}
+    WHERE RIDE_ID != 'ride_id'
+    limit 10
+)
+SELECT
+*
+FROM BIKE
